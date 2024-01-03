bac1d3f0f1381e8df4c220bd93cac18e21993c31
MKovza <michaelkovzanovich@gmail.com>
2024-01-03 16:27:02 +0300

    cr: remove comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Home from '@/app/(withNavigation)/(withModal)/home/page'
 import WithNavigationLayout from '@/app/(withNavigation)/layout'
-// import NiceModal from '@ebay/nice-modal-react'
 
 export default function Main() {
   return (
